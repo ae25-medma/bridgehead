a8ec2df17a3e48cf263bd5ac0ab80ef3b8aaf2c1
ae13-abcgen <150427868+ae13-abcgen@users.noreply.github.com>
2024-07-24 15:27:54 +0200

    update Sender URL to Local Sender

--- standort_preprocessing/file_watcher.py
+++ standort_preprocessing/file_watcher.py
@@ -11,8 +11,6 @@ load_dotenv()
 
 DIRECTORY_TO_WATCH = os.getenv('DIRECTORY_TO_WATCH')
 POST_URL = os.getenv('POST_URL')
-USERNAME_UNI = os.getenv('USERNAME_UNI')
-PASSWORD_UNI = os.getenv('PASSWORD_UNI')
 
 class NewFileHandler(FileSystemEventHandler):
     def on_created(self, event):
@@ -30,13 +28,13 @@ class NewFileHandler(FileSystemEventHandler):
                     POST_URL,
                     files=files,
                     headers=headers,
-                    auth=HTTPBasicAuth(USERNAME_UNI, PASSWORD_UNI),
+                    # auth=HTTPBasicAuth(USERNAME_UNI, PASSWORD_UNI),
                     verify=True  # Disable SSL verification
                 )
                 if response.status_code == 200:
                     print(f"File {filename} uploaded successfully.")
                 else:
-                    print(f"Failed to upload {filename}. Status code: {response.status_code}  \nIp: {POST_URL}\nUsername: {USERNAME_UNI}\nPassword:{PASSWORD_UNI}")
+                    print(f"Failed to upload {filename}. Status code: {response.status_code}  \nIp: {POST_URL}")
             except requests.exceptions.RequestException as e:
                 print(f"An error occurred: {e}")
 
