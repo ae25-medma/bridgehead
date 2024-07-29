4759cfa151c77043d81f4dd2425eda1aade73087
AdriEstev-PreMed <adrian.estevez@abcgen.ai>
2024-07-29 15:05:52 +0200

    Update file_watcher.py

--- standort_preprocessing/file_watcher.py
+++ standort_preprocessing/file_watcher.py
@@ -1,6 +1,7 @@
 import os
 import time
 import requests
+import logging
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from dotenv import load_dotenv
@@ -11,6 +12,7 @@ load_dotenv()
 
 DIRECTORY_TO_WATCH = os.getenv('DIRECTORY_TO_WATCH')
 POST_URL = os.getenv('POST_URL')
+logging.basicConfig(level=logging.INFO)
 
 class NewFileHandler(FileSystemEventHandler):
     def on_created(self, event):
@@ -33,19 +35,19 @@ class NewFileHandler(FileSystemEventHandler):
                     verify=True  # Disable SSL verification
                 )
                 if response.status_code == 200:
-                    print(f"File {filename} uploaded successfully.")
+                    logging.info(f"File {filename} uploaded successfully.")
                 else:
-                    print(f"Failed to upload {filename}. Status code: {response.status_code}  \nIp: {POST_URL}")
+                    logging.info(f"Failed to upload {filename}. Status code: {response.status_code}  \nIp: {POST_URL}")
             except requests.exceptions.RequestException as e:
                 print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    print("File-watcher started")
+    logging.info("File-watcher started")
     event_handler = NewFileHandler()
     observer = Observer()
     observer.schedule(event_handler, DIRECTORY_TO_WATCH, recursive=False)
     
-    print(f"Watching directory: {DIRECTORY_TO_WATCH}")
+    logging.info(f"Watching directory: {DIRECTORY_TO_WATCH}")
     
     observer.start()
     try:
