d10d66478569959482b54318d9e0af162b82320b
AdriEstev-PreMed <adrian.estevez@abcgen.ai>
2024-07-29 18:17:12 +0200

    Update start.sh

--- LocalServer/start.sh
+++ LocalServer/start.sh
@@ -12,6 +12,12 @@ TARGET_DIR="/home/ubuntu/"
 # Name of the folder to be copied (same as the script directory name)
 FOLDER_NAME="$(basename "$SCRIPT_DIR")"
 
+# Remove the existing directory in the target location if it exists
+if [ -d "$TARGET_DIR/$FOLDER_NAME" ]; then
+  echo "Removing existing directory $TARGET_DIR/$FOLDER_NAME"
+  rm -rf "$TARGET_DIR/$FOLDER_NAME"
+fi
+
 # Perform the copy operation
 cp -r "$PARENT_DIR/$FOLDER_NAME" "$TARGET_DIR"
 
